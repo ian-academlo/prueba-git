563cb38ee83d6a63715951f504fa80eb2a2f7cdf
JesusEscalonaC <jesusdescalonac@gmail.com>
2023-02-03 23:17:24 -0400

    first commit

---
+++ src/models/jesus.models.js
@@ -0,0 +1,32 @@
+const database = require("../utils/database");
+const { DataTypes } = require("sequelize");
+
+const Jesus = database.define("jesus", {
+  id: {
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  friendName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "friend_name",
+  },
+  friendLastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "friend_lastname",
+  },
+  friendAge: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: "friend_age",
+  },
+  friendLastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: "friedn_number",
+  },
+});
+
+module.exports = Jesus;
