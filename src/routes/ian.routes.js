e898ac140dc1ab53e201d334c0105c25c7a0b9af
Ian Rosas <ian.rosas@academlo.com>
2023-02-03 18:04:07 -0600

    add get all friends

--- src/routes/ian.routes.js
+++ src/routes/ian.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { getFriendById } = require("../controllers/ian.controllers");
+const { getFriendById, getFriends } = require("../controllers/ian.controllers");
 
 const router = Router();
 
+router.get("/friends", getFriends);
 router.get("/friends/:id", getFriendById);
 
 module.exports = router;
